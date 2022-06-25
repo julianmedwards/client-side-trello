15b27c57775f8b6fcbac9ecb3ad8e1589e8024f2
julianmedwards <julian.michael.edwards@gmail.com>
2022-06-25 15:49:16 -0500

    Expanding toggleable add lane form.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
+const icons = {
+    plus: 'fa-plus',
+    times: 'fa-times',
+}
+
 const BoardObj = function () {
     this.boardDiv = document.getElementById('board')
     this.lanes = {}
-    this.addLane = addLane
+    this.toggleAddingLane = toggleAddingLane
     this.addCard = addCard
     this.buttons = initBtns(this)
 }
@@ -10,7 +15,10 @@ function initBtns(board) {
     let buttons = document.getElementsByName('add-btn')
     for (let button of buttons) {
         if ((button.parentElement = board.boardDiv)) {
-            button.addEventListener('click', board.addLane)
+            button.lastElementChild.addEventListener(
+                'click',
+                board.toggleAddingLane
+            )
         } else {
             button.addEventListener('click', board.addCard)
         }
@@ -18,12 +26,38 @@ function initBtns(board) {
     return buttons
 }
 
-function addLane() {
-    console.log('Add a lane!')
+function toggleAddingLane() {
+    console.log('Preparing to add a lane!')
+    toggleVisiblity(this.previousElementSibling)
+    this.previousElementSibling['lane-name'].focus()
+    toggleIcon(this, icons.plus, icons.times)
+    // let newLane = document.createElement('div')
+    // newLane.classList.add('lane')
+    // this.before(newLane)
 }
 
 function addCard() {
     console.log('Add a card!')
+}
+
+function toggleVisiblity(element) {
+    if (
+        element.classList.contains('hidden') ||
+        element.classList.contains('visible')
+    ) {
+        element.classList.toggle('hidden')
+        element.classList.toggle('visible')
+    } else {
+        element.classList.add('hidden')
+    }
+}
+
+function toggleIcon(iconEl, initIcon, altIcon) {
+    if (iconEl.classList.contains(initIcon)) {
+        iconEl.classList.replace(initIcon, altIcon)
+    } else {
+        iconEl.classList.replace(altIcon, initIcon)
+    }
 }
 
 const board01 = new BoardObj()
